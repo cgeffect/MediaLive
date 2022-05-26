2f075c75069472aaa28e709564c3681b5b47b000
Jason <JasonBee@163.com>
2022-05-26 18:23:00 +0800

    整理

--- CGLive/RTMPEngine/Controller/RTMPPush.cpp
+++ CGLive/RTMPEngine/Controller/RTMPPush.cpp
@@ -10,7 +10,7 @@
 namespace mogic {
 int RTMPPush::initRTMP(const char *outPath, const char *filePath) {
     this->outPath = outPath;
-    int ret, i;
+    int ret;
 
     avformat_network_init();
     //Input
@@ -27,17 +27,17 @@ int RTMPPush::initRTMP(const char *outPath, const char *filePath) {
     }
     printf( "out_filename: %s\n", outPath);
     
-    for(i=0; i<ifmtCtx->nb_streams; i++) {
+    for(int i = 0; i < ifmtCtx->nb_streams; i++) {
         AVStream *stream = ifmtCtx->streams[i];
         AVCodecParameters *param = stream->codecpar;
-        if(param->codec_type==AVMEDIA_TYPE_VIDEO){
+        if (param->codec_type == AVMEDIA_TYPE_VIDEO) {
             videoIndex=i;
             break;
         }
     }
-    for(i=0; i<ifmtCtx->nb_streams; i++) {
-        if(ifmtCtx->streams[i]->codecpar->codec_type==AVMEDIA_TYPE_AUDIO){
-            audioIndex=i;
+    for(int i = 0; i < ifmtCtx->nb_streams; i++) {
+        if (ifmtCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+            audioIndex = i;
             break;
         }
     }
@@ -55,38 +55,37 @@ int RTMPPush::initRTMP(const char *outPath, const char *filePath) {
         stop();
         return -1;
     }
+    
     ofmt = ofmtCtx->oformat;
     
-    printf("name:%s, long_name:%s, mime_type:%s, extensions:%s\n",
-           ofmt->name, ofmt->long_name, ofmt->mime_type, ofmt->extensions);
+    printf("name:%s, long_name:%s, mime_type:%s, extensions:%s\n", ofmt->name, ofmt->long_name, ofmt->mime_type, ofmt->extensions);
     
-    for (i = 0; i < ifmtCtx->nb_streams; i++) {
-        
-        AVStream *in_stream = ifmtCtx->streams[i];
-        if(in_stream->codecpar->codec_type != AVMEDIA_TYPE_VIDEO && in_stream->codecpar->codec_type != AVMEDIA_TYPE_AUDIO) {
+    for (int i = 0; i < ifmtCtx->nb_streams; i++) {
+        AVStream *inStream = ifmtCtx->streams[i];
+        if(inStream->codecpar->codec_type != AVMEDIA_TYPE_VIDEO && inStream->codecpar->codec_type != AVMEDIA_TYPE_AUDIO) {
             continue;
         }
-        AVCodec *in_codec = avcodec_find_decoder(in_stream->codecpar->codec_id);
-        AVStream *out_stream = avformat_new_stream(ofmtCtx, NULL);
-        if (!out_stream) {
+        AVCodec *inCodec = avcodec_find_decoder(inStream->codecpar->codec_id);
+        AVStream *outStream = avformat_new_stream(ofmtCtx, NULL);
+        if (!outStream) {
             printf( "Failed allocating output stream\n");
             ret = AVERROR_UNKNOWN;
             stop();
             return -1;
         }
-        AVCodecContext *codec_ctx = avcodec_alloc_context3(in_codec);
-        ret = avcodec_parameters_to_context(codec_ctx, in_stream->codecpar);
+        AVCodecContext *codecCtx = avcodec_alloc_context3(inCodec);
+        ret = avcodec_parameters_to_context(codecCtx, inStream->codecpar);
         if (ret < 0){
             printf("Failed to copy in_stream codecpar to codec context\n");
             stop();
             return -1;
         }
          
-        codec_ctx->codec_tag = 0;
+        codecCtx->codec_tag = 0;
         if (ofmtCtx->oformat->flags & AVFMT_GLOBALHEADER) {
-            codec_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+            codecCtx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
         }
-        ret = avcodec_parameters_from_context(out_stream->codecpar, codec_ctx);
+        ret = avcodec_parameters_from_context(outStream->codecpar, codecCtx);
         if (ret < 0){
             printf("Failed to copy codec context to out_stream codecpar context\n");
             stop();
@@ -94,15 +93,12 @@ int RTMPPush::initRTMP(const char *outPath, const char *filePath) {
         }
     }
     
-    //Dump Format------------------
     av_dump_format(ofmtCtx, 0, outPath, 1);
     //Open output URL
     if (!(ofmt->flags & AVFMT_NOFILE)) {
         AVDictionary *format_opts = NULL;
         av_dict_set(&format_opts, "rw_timeout",  "1000000", 0); //设置超时时间,单位mcs
         avio_open2( &ofmtCtx->pb, outPath , AVIO_FLAG_WRITE , NULL , &format_opts);
-
-
 //        ret = avio_open(&ofmtCtx->pb, outPath, AVIO_FLAG_WRITE);
         if (ret < 0) {
             printf( "Could not open output URL '%s'", outPath);
@@ -162,40 +158,40 @@ int RTMPPush::writerTrailer() {
 
 int RTMPPush::writerPacket(AVPacket pkt) {
     int ret = 0;
-    AVStream *in_stream, *out_stream;
 
     //FIX：No PTS (Example: Raw H.264)
     //Simple Write PTS
-    if(pkt.pts==AV_NOPTS_VALUE){
+    if(pkt.pts == AV_NOPTS_VALUE) {
         //Write PTS
-        AVRational time_base1=ifmtCtx->streams[videoIndex]->time_base;
+        AVRational time_base1 = ifmtCtx->streams[videoIndex]->time_base;
         //Duration between 2 frames (us)
-        int64_t calc_duration=(double)AV_TIME_BASE/av_q2d(ifmtCtx->streams[videoIndex]->r_frame_rate);
+        int64_t calc_duration = (double)AV_TIME_BASE/av_q2d(ifmtCtx->streams[videoIndex]->r_frame_rate);
         //Parameters
-        pkt.pts=(double)(frameIndex*calc_duration)/(double)(av_q2d(time_base1)*AV_TIME_BASE);
-        pkt.dts=pkt.pts;
-        pkt.duration=(double)calc_duration/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+        pkt.pts = (double)(frameIndex*calc_duration)/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+        pkt.dts = pkt.pts;
+        pkt.duration = (double)calc_duration/(double)(av_q2d(time_base1)*AV_TIME_BASE);
     }
+    
     //Important:Delay
-    if(pkt.stream_index==videoIndex){
-        AVRational time_base=ifmtCtx->streams[videoIndex]->time_base;
-        AVRational time_base_q={1,AV_TIME_BASE};
-        int64_t pts_time = av_rescale_q(pkt.dts, time_base, time_base_q);
-        int64_t now_time = av_gettime() - startTime;
-        if (pts_time > now_time) {
-            av_usleep((int)(pts_time - now_time));
+    if(pkt.stream_index == videoIndex) {
+        AVRational timeBase = ifmtCtx->streams[videoIndex]->time_base;
+        AVRational timeBaseQ = {1, AV_TIME_BASE};
+        int64_t ptsTime = av_rescale_q(pkt.dts, timeBase, timeBaseQ);
+        int64_t nowTime = av_gettime() - startTime;
+        if (ptsTime > nowTime) {
+            av_usleep((int)(ptsTime - nowTime));
         }
     }
 
-    in_stream  = ifmtCtx->streams[pkt.stream_index];
-    out_stream = ofmtCtx->streams[pkt.stream_index];
+    AVStream *inStream  = ifmtCtx->streams[pkt.stream_index];
+    AVStream *outStream = ofmtCtx->streams[pkt.stream_index];
     /* copy packet */
     //Convert PTS/DTS
-    pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, AVRounding(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-    pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, AVRounding(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-    pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+    pkt.pts = av_rescale_q_rnd(pkt.pts, inStream->time_base, outStream->time_base, AVRounding(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+    pkt.dts = av_rescale_q_rnd(pkt.dts, inStream->time_base, outStream->time_base, AVRounding(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+    pkt.duration = av_rescale_q(pkt.duration, inStream->time_base, outStream->time_base);
     pkt.pos = -1;
-    //Print to Screen
+    
     if(pkt.stream_index==videoIndex){
         printf("Send %8d %8lld video frames to output %s\n",frameIndex, pkt.pts, outPath);
         frameIndex++;
@@ -204,6 +200,7 @@ int RTMPPush::writerPacket(AVPacket pkt) {
         printf("Send %8d audio frames to output %s\n",pcmIndex, outPath);
         pcmIndex++;
     }
+    
     //ret = av_write_frame(ofmt_ctx, &pkt);
     ret = av_interleaved_write_frame(ofmtCtx, &pkt);
 
@@ -214,6 +211,7 @@ int RTMPPush::writerPacket(AVPacket pkt) {
 }
 
 void RTMPPush::stop() {
+    printf("RTMPPush::stop\n");
     if (ifmtCtx) {
         avformat_close_input(&ifmtCtx);
         ifmtCtx = nullptr;
