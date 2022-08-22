fa99beec2513c942edd838561c9734606af959e8
Jason <JasonBee@163.com>
2022-08-22 14:46:25 +0800

    推流264/flv, 拉流flv

---
+++ CGLive/RTMPEngine/RTMPDump/send_flv/simplest_librtmp_sendflv.h
@@ -0,0 +1,32 @@
+//
+//  simplest_librtmp_sendflv.h
+//  CGLive
+//
+//  Created by Jason on 2022/8/22.
+//
+
+#ifndef simplest_librtmp_sendflv_h
+#define simplest_librtmp_sendflv_h
+
+
+#ifdef __APPLE__
+
+//cpp
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//C symbol declarations
+
+int send_flv(char* argv);
+
+//C symbol declarations
+
+#ifdef __cplusplus
+}
+#endif
+//cpp
+
+#endif
+
+#endif /* simplest_librtmp_sendflv_h */
