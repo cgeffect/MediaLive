c95c7ebda57a288877fdaaf05c4ba1489e446c0a
Jason <JasonBee@163.com>
2022-08-22 15:01:40 +0800

    整理

---
+++ CGLive/LivePlayer/LivePlayer.hpp
@@ -0,0 +1,13 @@
+//
+//  LivePlayer.hpp
+//  CGLive
+//
+//  Created by Jason on 2022/8/22.
+//
+
+#ifndef LivePlayer_hpp
+#define LivePlayer_hpp
+
+#include <stdio.h>
+
+#endif /* LivePlayer_hpp */
---
+++ CGLive/LivePush/LivePush.hpp
@@ -0,0 +1,13 @@
+//
+//  LivePush.hpp
+//  CGLive
+//
+//  Created by Jason on 2022/8/22.
+//
+
+#ifndef LivePush_hpp
+#define LivePush_hpp
+
+#include <stdio.h>
+
+#endif /* LivePush_hpp */
